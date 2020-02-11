1aacc551a4092b088f1902ef56c5df9f04fbd5ae
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-11 09:26:52 -0800

    add all the components, just need to finish styling

--- chocolate-pizza/src/App.js
+++ chocolate-pizza/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
-import Directions from './Directions.js'
+import Directions from './Directions.js';
+import Recipe from './Recipe.js';
+import Footer from './Footer.js';
 import { paragraph1, paragraph2, paragraph3 } from './paragraphs.js'
+
 
 import './App.css';
 import { render } from '@testing-library/react';
@@ -15,8 +18,12 @@ render() {
     <div className="App">
 
     <Header logo={'/assets/logo.png'} fb={'/assets/fb-icon.png'} google={'/assets/gp-icon.png'} flic={'/assets/flic-icon.png'} insta={'/assets/insta-icon.png'} mail={'/assets/mail-icon.png'} twitter={'/assets/twit-icon.png'} rss={'/assets/rss-icon.png'} ></Header>
-    
+
     <Directions mainPhoto={'/assets/choco-pizza.png'} directions1={paragraph1} directions2={paragraph2} directions3={paragraph3} ></Directions>
+
+    <Recipe></Recipe>
+
+    <Footer vanessa={'/assets/van-pic.png'}></Footer>
     
     </div>
   )
---
+++ chocolate-pizza/src/Recipe.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Recipe extends Component {
+    render() {
+        return (
+            <div className='recipe-div'>
+                <ul>
+                    <li>1 1/2 cups milk</li>
+                    <li>1/2 cup mascarpone</li>
+                    <li>1/2 tsp pink salt</li>
+                    <li>1 lb Black Mission Figs</li>
+                    <li>1/2 cup brown sugar</li>
+                    <li>2-4 tbsp water</li>
+                    <li>1 1/2 cups heavy cream</li>
+                    <li>1/3 granulated sugar</li>
+                    <li>2 egg yolks</li>
+                    <li>1 lemon, juiced</li>
+                    <li>2 tbsp butter</li>
+                    <li>1 cup honey roasted pecans, roughly chopped</li>
+                </ul>
+            </div>
+        )
+    }
+}
